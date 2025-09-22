f9e566f6504c04bd8d4faba0a3c2e7f7468fdbfc
Alberth Brendon <alberth.brendon@gmail.com>
2025-09-22 20:34:29 -0300

    Second

---
+++ src/AnotationClass.java
@@ -0,0 +1,11 @@
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+
+
+public @interface AnotationClass {
+
+    String value();
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+@AnotationClass("ANOTATION TEST EBAC EXERCICIO 01")
+public class Test {
+
+    public void printAnotation(){
+
+    }
+}
